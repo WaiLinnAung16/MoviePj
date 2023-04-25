f7161246eba27491bb7c57434e6df419cac7e53d
WaiLinnAung18 <waia00198@gmail.com>
2023-04-25 15:18:09 +0630

    icon change and logo text change

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -34,7 +34,7 @@ const Navbar = () => {
             <h1 className="relative group cursor-pointer">
               <span className="flex items-center gap-1 z-20 font-extrabold text-2xl text-white bg-slate-900 px-3 py-1 relative transition-all duration-500 -skew-y-3 group-hover:-translate-x-1 group-hover:-translate-y-1">
                 <RiMovie2Line />
-                YinShote
+                Moviezine
               </span>
               <span className="-z-10 after:block after:absolute after:-inset-0 after:-skew-y-3  after:border-2 after:border-slate-800"></span>
             </h1>
